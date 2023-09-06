cf0f60de7e20d1d208ba505988d8d66b22db2d08
Candy Tsai <candy02058912@gmail.com>
2023-09-06 16:11:02 +0000

    Add better styling and info display

--- public/script.js
+++ public/script.js
@@ -2,13 +2,45 @@
 const params = new URLSearchParams(window.location.search);
 const id = params.get('id');
 
+function displayInfo(data) {
+  const infoDiv = document.getElementById('info');
+
+  const card = document.createElement('div');
+  card.className = 'ticket-card';
+
+  for (const [key, value] of Object.entries(data)) {
+    const row = document.createElement('div');
+    row.className = 'ticket-row';
+
+    const label = document.createElement('div');
+    label.className = 'ticket-label';
+    label.textContent = key;
+
+    const valueDiv = document.createElement('div');
+    valueDiv.className = 'ticket-value';
+    valueDiv.textContent = value;
+
+    row.appendChild(label);
+    row.appendChild(valueDiv);
+    card.appendChild(row);
+  }
+
+  infoDiv.appendChild(card);
+
+
+  const checkinButton = document.createElement('button');
+  checkinButton.className = 'checkin-button';
+  checkinButton.textContent = 'Check In';
+
+}
+
+
 // Fetch the data from the backend for this "id"
 fetch(`/data?id=${id}`)
   .then(response => response.json())
   .then(data => {
     if (data.success) {
-      // Display the information
-      document.getElementById('info').innerText = `Information for ID ${id}: ${JSON.stringify(data.data)}`;
+      displayInfo(data.data);
     } else {
       document.getElementById('info').innerText = 'ID not found';
     }
